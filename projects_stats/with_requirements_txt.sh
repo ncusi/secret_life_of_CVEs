19f5173348dfce005858fe5110f1ea3043678a9b
Jakub Narębski <jnareb@mat.umk.pl>
2022-10-22 00:48:06 +0200

    Extend with_requirements_txt.sh script
    Add some links to documentation, missing commands for *in_subdirectory*
    version, find the union.

--- projects_stats/with_requirements_txt.sh
+++ projects_stats/with_requirements_txt.sh
@@ -11,7 +11,10 @@
 # - c = Commit
 # - p = Project
 # - P = Forked/Root Project
-
+#
+# See also:
+# - https://github.com/woc-hack/tutorial
+# - https://bitbucket.org/swsc/lookup/src/master/tutorial.md
 
 # find number of all commits that changed 'requirements.txt' file,
 # either in top level of repository, or in some subdirectory,
@@ -25,12 +28,18 @@ zgrep -c -E "/requirements\.txt;" /da?_data/basemaps/gz/f2c*.s \
 # to be later used to find which projects they belong to
 zgrep -h -E "^requirements\.txt;" /da?_data/basemaps/gz/f2c*.s \
       >search.top_level_requirements.commits.out
+zgrep -h -E "/requirements\.txt;" /da?_data/basemaps/gz/f2c*.s \
+      >search.in_subdirectory_requirements.commits.out
 
 # find the projects that those commits belong to,
 # that is all projects that have 'requirements.txt' file
 cat search.top_level_requirements.commits.out |
     cut -d ';' -f2 |
     ~/lookup/getValues c2P  >search.top_level_requirements.c2P.out
+cat search.in_subdirectory_requirements.commits.out |
+    cut -d ';' -f2 |
+    ~/lookup/getValues c2P  >search.in_subdirectory_requirements.c2P.out
+
 
 # extract set of individual projects,
 # taking into account repetition, and multiple projects as result
@@ -40,3 +49,20 @@ cat search.top_level_requirements.c2P.out |
     cut -d';' -f2- |
     sed 's/;/\n/' |
     sort | uniq -c  >search.top_level_requirements.c2P-P_counts.v2.out
+cat search.in_subdirectory_requirements.c2P.out |
+    cut -d';' -f2- |
+    sed 's/;/\n/' |
+    sort | uniq -c  >search.in_subdirectory_requirements.c2P-P_counts.v2.out
+
+# find common projects, that is projects that have both top level 'requirements.txt'
+# at some point of time, and 'requirememnts.txt' in a subdirectory at some possibly
+# different point of time
+cut -c9- search.top_level_requirements.c2P-P_counts.v2.out \
+    >search.top_level_requirements.c2P-P_list.v2.out
+cut -c9- search.in_subdirectory_requirements.c2P-P_counts.v2.out \
+    >search.in_subdirectory_requirements.c2P-P_list.v2.out
+
+comm -1 -2 \
+     search.top_level_requirements.c2P-P_list.v2.out \
+     search.in_subdirectory_requirements.c2P-P_list.v2.out \
+     >search.comm_requirements.c2P-P_list.v2.out
