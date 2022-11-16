68ddfb6209781f047ad4c5212487ca73eda3fee2
Mikołaj Fejzer <mfejzer@mat.umk.pl>
2022-11-16 21:21:21 +0100

    cve_search_parser.py: Add simple project name extraction via oscar

--- dependency_search/cve_search_parser.py
+++ dependency_search/cve_search_parser.py
@@ -9,6 +9,7 @@ import sys
 
 import pandas as pd
 
+from oscar import Commit
 
 def main():
     cve_search_filename = sys.argv[1]
@@ -16,10 +17,11 @@ def main():
     column_names = ['commit', 'tree', 'parent', 'author', 'commiter', 'author_time', 'commiter_time', 'author_timezone',
                     'commiter_timezone', 'commit_message']
     df = pd.read_csv(cve_search_filename, sep=";", encoding='latin-1', names=column_names)
-    df = find_cve(df)
-    extracted_df = df[['commit', 'cve']]
-    # print(extracted_df.columns)
-    # print(extracted_df.head())
+    extracted_df = find_cve(df)
+    extracted_df = extracted_df[['commit', 'cve']]
+    extracted_df = find_project_names(extracted_df.head())
+    print(extracted_df.columns)
+    print(extracted_df.head())
     extracted_df.to_parquet(dataframe_filename)
 
 
@@ -32,6 +34,21 @@ def find_cve(df):
     pattern = r'(CVE-\d{4}-\d{4,7})'
     df['cve'] = df['commit_message'].str.extract(pattern)
     return df
+
+
+def find_project_names(df):
+    print(df)
+    df['project_name'] = df['commit'].apply(find_project_name)
+    return df
+
+def find_project_name(commit_sha):
+    print(commit_sha)
+    commit = Commit(commit_sha)
+    tmp = []
+    for project_name in commit.project_names:
+        tmp.append(project_name)
+    print(tmp)
+    return tmp[0]
 
 
 if __name__ == '__main__':
