09c2a8ead56bd9ebca13ee38222b78d29dcb3dcb
Mikołaj Fejzer <mfejzer@mat.umk.pl>
2022-12-07 19:29:02 +0100

    retrieve_cve_dates.py: Replace urllib with urllib3, store errors in same df

--- dependency_search/retrieve_cve_dates.py
+++ dependency_search/retrieve_cve_dates.py
@@ -9,7 +9,7 @@ Saves two dataframes, first for retrieved publish dates of cves, second for erro
 """
 import json
 import sys
-import urllib.request
+import urllib3
 
 import pandas as pd
 from tqdm import tqdm
@@ -22,34 +22,32 @@ def main():
     time_df = df[['commit', 'commit_cves', 'commiter_time', 'author_time', 'project_names']]
     exploded_time_df = time_df.explode('commit_cves')
     unique_cves = exploded_time_df['commit_cves'].dropna().unique()
-    result, errors = download_cve_published_date(unique_cves)
-    cve_published_date_df = pd.DataFrame(result, columns=['cve', 'published_date'])
+    result = download_cve_published_date(unique_cves)
+    cve_published_date_df = pd.DataFrame(result, columns=['cve', 'published_date', 'error'])
     cve_published_date_df.to_parquet(cve_published_date_filename)
-    cve_published_date_errors_df = pd.DataFrame(result, columns=['cve', 'error'])
-    cve_published_date_errors_df.to_parquet(cve_published_date_filename + '_errors')
 
 
 def download_cve_published_date(unique_cves):
     """
     Downloads each cve details via rest api from instance of CVE-Search
     :param unique_cves: list of unique cve
-    :return: a pair of lists, first element is list of downloaded cve publish dates, second list of errors
+    :return: list of results (cve, published date, error)
     """
+    http = urllib3.PoolManager()
     result = []
-    errors = []
     for cve in tqdm(unique_cves):
         try:
-            cve_published_date = gather_cve_published_data(cve)
-            result.append((cve, cve_published_date))
+            cve_published_date = gather_cve_published_data(http, cve)
+            result.append((cve, cve_published_date, None))
         except Exception as err:
             print(cve)
             print(err)
-            errors.append((cve, err))
+            result.append((cve, None, err))
             pass
-    return result, errors
+    return result
 
 
-def gather_cve_published_data(cve):
+def gather_cve_published_data(http, cve):
     """
     Retrieves cve details via rest api from instance of CVE-Search
     :param cve: Unique cve in format CVE-\d{4}-\d{4,7}, for example CVE-2014-2972
@@ -57,10 +55,9 @@ def gather_cve_published_data(cve):
     """
     url = 'http://158.75.112.151:5000/api/cve/'
     request_url = url + cve
-    with urllib.request.urlopen(request_url) as result:
-        data = json.load(result)
-        return data['Published']
-
+    response = http.request("GET", request_url)
+    published = response.json()['Published']
+    return published
 
 if __name__ == '__main__':
     main()
