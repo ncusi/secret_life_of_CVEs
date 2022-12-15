ncusi/secret_life_of_CVEs

commit 35465e7797e6153a5006d0a89d07c5a05f338c8f

retrieve_cve_info.py: Retrieve more data (+ dvc run)

dependency_search/retrieve_cve_info.py
@@ -44,8 +44,9 @@ def main():
 
     result = download_cve_published_date(unique_cve)
 
-    cve_published_date_df = pd.DataFrame(result,
-                                         columns=['cve', 'cvss', 'cwe', 'error']).convert_dtypes()
+    cve_published_date_df = pd.DataFrame\
+        .from_records(result)\
+        .convert_dtypes()
     cve_published_date_df.to_parquet(cve_dataframe_filename)
 
 
@@ -53,7 +54,7 @@ def download_cve_published_date(unique_cve):
     """
     Downloads each cve details via rest api from instance of CVE-Search
     :param unique_cve: list of unique cve
-    :return: list of results (cve, cvss, cve)
+    :return: list of dicts (including 'cvss', 'cwe', 'error' keys)
     """
     http = urllib3.PoolManager()
 
@@ -69,21 +70,36 @@ def gather_cve_published_data(http, cve):
     Retrieves cve details via rest api from instance of CVE-Search
     :param http: urllib-compatible object, with .request("GET", request_url) method
     :param cve: Unique cve in format CVE-\d{4}-\d{4,7}, for example CVE-2014-2972
-    :return: (cve, cvss, cws, error) tuple
+    :return: dict that includes at least 'cvss', 'cwe' and 'error' keys
     """
     url = 'http://158.75.112.151:5000/api/cve/'
     request_url = url + cve
-    cvss = None
-    cwe = None
-    error = None
+    result = {
+        'cvss': None,
+        'cwe': None,
+        'error': None,
+    }
     try:
         response = http.request("GET", request_url)
         data = json.loads(response.data.decode('utf-8'))
-        cvss = data['cvss']
-        cwe  = data['cwe']
+
+        result['cvss'] = data['cvss']
+        result['cwe']  = data['cwe']
+        result['cvss-vector'] = data['cvss-vector']  # example: "AV:L/AC:L/Au:N/C:N/I:N/A:P"
+        if "access" in data:
+            result["access.authentication"] = data["access"]["authentication"]
+            result["access.complexity"] = data["access"]["complexity"]
+            result["access.vector"] = data["access"]["vector"]
+        if "impact" in data:
+            result["impact.availability"] = data["impact"]["availability"]
+            result["impact.confidentiality"] = data["impact"]["confidentiality"]
+            result["impact.integrity"] = data["impact"]["integrity"]
+    except urllib3.exceptions.NewConnectionError as exception:
+        print(f"Connection error for {request_url}: {exception}")
+        sys.exit(1)
     except Exception as exception:
-        error = str(exception)
-    return cve, cvss, cwe, error
+        result['error'] = str(exception)
+    return result
 
 
 if __name__ == '__main__':

dvc.yaml
@@ -9,9 +9,9 @@ stages:
     - data/published_cve_df_filename
   retrieve_cve_info:
     cmd: python3 dependency_search/retrieve_cve_info.py data/published_cve_df_filename
-      data/unique_cve_info-cvss_cwe.parquet
+      data/unique_cve_info-cvss_cwe_etc.parquet
     deps:
     - data/published_cve_df_filename
     - dependency_search/retrieve_cve_info.py
     outs:
-    - data/unique_cve_info-cvss_cwe.parquet
+    - data/unique_cve_info-cvss_cwe_etc.parquet

commit 0a83894e07f81570a4ceafaf5e56481245edd7d5

retrieve_cve_info.py: Fix bug that make output missing 'cve' field
Big introduced in 35465e7 (retrieve_cve_info.py: Retrieve more data
(+ dvc run), 2022-12-14).

dependency_search/retrieve_cve_info.py
@@ -5,7 +5,7 @@
 
 Retrieves CVE information via REST API from an instance of CVE-Search
 Requires result of cve_search_parser.py, or any other dataframe with 'cve' column
-Saves dataframe with columns 'cve', 'cvss', 'cwe'
+Saves dataframe with columns 'cve', 'cvss', 'cwe', and possibly others
 
 Based on retrieve_cve_dates.py
 """
@@ -54,7 +54,7 @@ def download_cve_published_date(unique_cve):
     """
     Downloads each cve details via rest api from instance of CVE-Search
     :param unique_cve: list of unique cve
-    :return: list of dicts (including 'cvss', 'cwe', 'error' keys)
+    :return: list of dicts (including 'cve', 'cvss', 'cwe', 'error' keys)
     """
     http = urllib3.PoolManager()
 
@@ -70,11 +70,12 @@ def gather_cve_published_data(http, cve):
     Retrieves cve details via rest api from instance of CVE-Search
     :param http: urllib-compatible object, with .request("GET", request_url) method
     :param cve: Unique cve in format CVE-\d{4}-\d{4,7}, for example CVE-2014-2972
-    :return: dict that includes at least 'cvss', 'cwe' and 'error' keys
+    :return: dict that includes at least 'cve', 'cvss', 'cwe' and 'error' keys
     """
     url = 'http://158.75.112.151:5000/api/cve/'
     request_url = url + cve
     result = {
+        'cve': cve,
         'cvss': None,
         'cwe': None,
         'error': None,