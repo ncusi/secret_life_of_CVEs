9eac51a58816172527844a3d7bd5e9490248c67f
Mikołaj Fejzer <mfejzer@mat.umk.pl>
2022-12-29 21:58:31 +0100

    Add merge_cve_info_and_cve_lifespan.py

---
+++ dependency_search/merge_cve_info_and_cve_lifespan.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Usage: %(scriptName) <unique_cve_info-cvss_cwe_etc.parquet> <cve_lifespan_language_df> <cve_survival_input_df>
+
+Requires result of add_cvss_ranking.py and calculate_cve_lifespan_per_project.py
+"""
+import sys
+
+import pandas as pd
+
+
+def merge_dfs(unique_cve_info_cvss_cwe_etc_df, cve_lifespan_language_df_df):
+    commits_df = cve_lifespan_language_df_df.drop_duplicates()
+    commits_df['cve'] = commits_df['commit_cves']
+    merged_df = commits_df.merge(unique_cve_info_cvss_cwe_etc_df.drop('error', axis='columns'), on='cve')
+    return merged_df
+
+
+def main():
+    unique_cve_info_cvss_cwe_etc_df_filename = sys.argv[1]
+    cve_lifespan_language_df_filename = sys.argv[2]
+    cve_survival_input_df_filename = sys.argv[3]
+    unique_cve_info_cvss_cwe_etc_df = pd.read_parquet(unique_cve_info_cvss_cwe_etc_df_filename)
+    cve_lifespan_language_df_df = pd.read_parquet(cve_lifespan_language_df_filename)
+
+    cve_survival_input_df_ = merge_dfs(unique_cve_info_cvss_cwe_etc_df, cve_lifespan_language_df_df)
+
+    cve_survival_input_df_.to_parquet(cve_survival_input_df_filename)
+
+
+if __name__ == '__main__':
+    main()
