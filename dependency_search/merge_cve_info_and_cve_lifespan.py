6e3db987602b87976b62247b32208a3015639297
Mikołaj Fejzer <mfejzer@mat.umk.pl>
2023-01-14 19:47:12 +0100

    cve_survival_input_most_used_language_df_filename: Save two result dataframes
    Second one contains data only for most used language

--- dependency_search/merge_cve_info_and_cve_lifespan.py
+++ dependency_search/merge_cve_info_and_cve_lifespan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""Usage: %(scriptName) <unique_cve_info-cvss_cwe_etc.parquet> <cve_lifespan_language_df> <cve_survival_input_df>
+"""Usage: %(scriptName) <unique_cve_info-cvss_cwe_etc.parquet> <cve_lifespan_language_df> <cve_lifespan_df> <cve_survival_input_df> <cve_survival_input_most_used_language_df>
 
 Requires result of add_cvss_ranking.py and calculate_cve_lifespan_per_project.py
 """
@@ -20,14 +20,19 @@ def merge_dfs(unique_cve_info_cvss_cwe_etc_df, cve_lifespan_language_df_df):
 def main():
     unique_cve_info_cvss_cwe_etc_df_filename = sys.argv[1]
     cve_lifespan_language_df_filename = sys.argv[2]
-    cve_survival_input_df_filename = sys.argv[3]
+    cve_lifespan_df_filename = sys.argv[3]
+    cve_survival_input_df_filename = sys.argv[4]
+    cve_survival_input_most_used_language_df_filename = sys.argv[5]
+
     unique_cve_info_cvss_cwe_etc_df = pd.read_parquet(unique_cve_info_cvss_cwe_etc_df_filename)
-    cve_lifespan_language_df_df = pd.read_parquet(cve_lifespan_language_df_filename)
+    cve_lifespan_language_df = pd.read_parquet(cve_lifespan_language_df_filename)
+    cve_lifespan_df = pd.read_parquet(cve_lifespan_df_filename)
 
-    cve_survival_input_df_ = merge_dfs(unique_cve_info_cvss_cwe_etc_df, cve_lifespan_language_df_df)
+    cve_survival_input_df = merge_dfs(unique_cve_info_cvss_cwe_etc_df, cve_lifespan_language_df)
+    cve_survival_input_most_used_language_df = merge_dfs(unique_cve_info_cvss_cwe_etc_df, cve_lifespan_df)
 
-    cve_survival_input_df_.to_parquet(cve_survival_input_df_filename)
-
+    cve_survival_input_df.to_parquet(cve_survival_input_df_filename)
+    cve_survival_input_most_used_language_df.to_parquet(cve_survival_input_most_used_language_df_filename)
 
 if __name__ == '__main__':
     main()
